f6083c2934cb8ce5bc4356c40b32d958cd1e0b86
marswalkerr <sanketsavani004@gmail.com>
2024-07-20 12:57:16 +0530

    Create tweet function

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,4 +1,3 @@
-import mongoose, { isValidObjectId } from "mongoose"
 import { Tweet } from "../models/tweet.model.js"
 import { User } from "../models/user.model.js"
 import { ApiError } from "../utils/ApiError.js"
@@ -6,7 +5,24 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
+    const { content } = req.body;
+
+    const user = await User.findById(req.user?._id);
+
+    if (!user) {
+        throw new ApiError(400, "User does not exist");
+    }
+
+    const tweet = await Tweet.create({
+        content: content,
+        owner: user._id
+    })
+
+    const populateTweet = await Tweet.findById(tweet._id).populate("owner", "username avatar");
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, populateTweet, "Tweet created sucessfully"));
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
