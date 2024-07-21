6479d8e76ebea2f86701844fb194a7a4eb0bec98
marswalkerr <sanketsavani004@gmail.com>
2024-07-21 23:28:37 +0530

    Added function to get channel stats

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -8,6 +8,33 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const userId = req.user._id;
+
+    const totalSubscribers = await Subscription.countDocuments({ channel: userId });
+
+    const totalVideos = await Video.countDocuments({ owner: userId });
+
+    const videos = await Video.find({ owner: userId });
+    const totalViews = videos.reduce((acc, video) => acc + video.views, 0);
+
+    const totalLikes = await Like.countDocuments({
+        video: { $in: videos.map(video => video._id) }
+    });
+
+    const channelStats = {
+        totalSubscribers,
+        totalVideos,
+        totalViews,
+        totalLikes
+    };
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            channelStats,
+            "Channel stats retrieved successfully"
+        ));
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
